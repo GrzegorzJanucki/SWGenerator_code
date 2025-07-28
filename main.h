3176bd2ae335a332846dbf76ca209fb159928a68
GrzegorzJanucki <grzegorz.janucki@gmail.com>
2025-07-28 17:32:53 +0200

    v0.0

---
+++ main.h
@@ -0,0 +1,4 @@
+#define I2C1_PORT i2c1
+#define I2C1_SDA 2
+#define I2C1_SCL 3
+#define SLEEPTIME 25
---
+++ ssd1306_init.h
@@ -0,0 +1,8 @@
+#ifndef ssd1306_init_h
+#define ssd1306_init_h
+#pragma once// ** DEFINES **
+
+void initialization (void);
+void animation(void);
+
+#endif
